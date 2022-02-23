2da7d92cbb7309adf894c7b8ab76eb654b57f288
Eleonora Kazakova <abei-2005@yandex.ru>
2022-02-23 19:36:48 +0100

    Create first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./styles/App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-grid">
+      <div className="app-bar"></div>
+      <div className="app-hero"></div>
+      <div className="app-about"></div>
+      <div className="app-portfolio"></div>
+      <div className="app-tech"></div>
+      <div className="app-footer"></div>
     </div>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,5 @@
+import "../styles/hero.css";
+
+export default function Hero() {
+  return <div></div>;
+}
